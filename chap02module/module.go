04b2bd8401d10d42484d07df2d67e21cfd7830e4
Hang <huhuhang@users.noreply.github.com>
2023-08-24 15:38:01 +0800

    fix

--- chap02module/module.go
+++ chap02module/module.go
@@ -5,15 +5,12 @@ import (
 	"fmt"
 )
 
-// 输出 hello
 func Hello() {
 	fmt.Println("hello")
 }
 
-// 将输入的字符串转为 base64 编码
 func StringTobase64(inString string) {
-	// 编码为 base64
 	encodedString := base64.StdEncoding.EncodeToString([]byte(inString))
-	fmt.Printf("原字符串: %s\n", inString)
-	fmt.Printf("base64 编码后: %s\n", encodedString)
+	fmt.Printf("Original string: %s\n", inString)
+	fmt.Printf("After base64 encoding: %s\n", encodedString)
 }
